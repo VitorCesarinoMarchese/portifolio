9a9f3533dc2b2d90f3e77aeecce611762e83f3a4
Vitor Cesarino <84383797+VitorCesarinoMarchese@users.noreply.github.com>
2023-09-26 12:16:51 -0300

    Add files via upload

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,18 +1,46 @@
-import '../styles/NavBar.sass'
+import { useState } from "react";
+import hamburguer from "../assets/hamburguer.svg";
+import fillhamburguer from "../assets/fillhamburguer.svg";
+import "../styles/NavBar.sass";
 
-function NavBar(){
-    return(
-        <div className='nav'>
-            <nav>
-                <span>Vitor Cesarino Marchese</span>
-                    <ul>
-                        <li><a href="#Sobre">Sobre</a></li>
-                        <li><a href="#Projetos">Meus Projetos</a></li>
-                        <li><a href="#Contatos">Contatos</a></li>
-                    </ul>
-            </nav>
-        </div>
-    )
+function NavBar() {
+  const [active, SetActive] = useState(false);
+  return (
+    <div className="nav">
+      <nav>
+        <span>Vitor Cesarino Marchese</span>
+        <button
+          style={{ display: active ? "none" : "flex" }}
+          className="NavBtn"
+          onClick={() => SetActive(true)}
+        >
+          <img src={hamburguer} alt="menu" />
+        </button>
+        <ul
+        style={{display: active ? "flex" : "none"}}
+        >
+          <li>
+            <button>
+              <img
+                src={fillhamburguer}
+                alt=""
+                onClick={() => SetActive(false)}
+              />
+            </button>
+          </li>
+          <li>
+            <a href="#Sobre">Sobre</a>
+          </li>
+          <li>
+            <a href="#Projetos">Meus Projetos</a>
+          </li>
+          <li>
+            <a href="#Contatos">Contatos</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
 
-export default NavBar
+export default NavBar;
