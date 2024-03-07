682870b0a78a34cab9ca3a0131f7ff0484d0db4c
Vitor Cesarino <84383797+VitorCesarinoMarchese@users.noreply.github.com>
2024-03-06 21:01:24 -0300

    Links

--- src/components/Projetos.tsx
+++ src/components/Projetos.tsx
@@ -6,32 +6,36 @@ function Projetos() {
   return (
     <div className="Projetos" id="Projetos">
       <h2>Meus Projetos</h2>
-      <div className="Projeto">
-        <img src={ToGoApp} alt="" />
-        <div className="Detalhes">
-          <h3>ToGoApp</h3>
-          <p>
-            Um site com uma To do list que utilizei para aprender o
-            funcionamento de um CRUD{"(create, read, update)"} na linguagem GO,
-            alem de aprender a implementar meu backend com fron-end.
-          </p>
-          <span>Status</span>
-          <p className="danger">Em desenvolvimento</p>
+      <a href="https://github.com/VitorCesarinoMarchese/ToGoApp">
+        <div className="Projeto">
+          <img src={ToGoApp} alt="" />
+          <div className="Detalhes">
+            <h3>ToGoApp</h3>
+            <p>
+              Um site com uma To do list que utilizei para aprender o
+              funcionamento de um CRUD{"(create, read, update)"} na linguagem GO,
+              alem de aprender a implementar meu backend com fron-end.
+            </p>
+            <span>Status</span>
+            <p className="danger">Em desenvolvimento</p>
+          </div>
         </div>
-      </div>
-      <div className="Projeto mt">
-        <img src={Floricultura} alt="" />
-        <div className="Detalhes">
-          <h3>RoseMarry</h3>
-          <p>
-            Um site em que utilizei sua criacao para aprender o basico de como
-            React e Sass funcionam, utilizando o react-router-dom para gerenciar
-            as rotas.
-          </p>
-          <span>Status</span>
-          <p className="success">Concluido</p>
+      </a>
+      <a href="https://github.com/VitorCesarinoMarchese/FloriculturaReact">
+        <div className="Projeto mt">
+          <img src={Floricultura} alt="" />
+          <div className="Detalhes">
+            <h3>RoseMarry</h3>
+            <p>
+              Um site em que utilizei sua criacao para aprender o basico de como
+              React e Sass funcionam, utilizando o react-router-dom para gerenciar
+              as rotas.
+            </p>
+            <span>Status</span>
+            <p className="success">Concluido</p>
+          </div>
         </div>
-      </div>
+      </a>
     </div>
   );
 }
